25fdb9c43b11e333b0f6740024a482f5e666b0c0
Flavia Schnaider <schflavia@hotmail.com>
2025-06-20 13:23:20 -0300

    alterado api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+// export const api = axios.create({
+//   baseURL: 'http://localhost:5000/api'
+// });
+
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://eventnow.onrender.com/api'  // substitua se necessário
 });
